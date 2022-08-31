535eff8d754e5b7bea0b05396a2d47be9ec65a00
calic4t <110143911+calic4t@users.noreply.github.com>
2022-08-31 17:50:59 +1000

    Update digimaker.js

--- digimaker.js
+++ digimaker.js
@@ -1,1 +1,27 @@
+function replaceSidebar () {
+  //store the new version of the button and the old version
+  let oldElement2 = document.getElementsByClassName("main-aside")[0];
+  let newElement2 = oldElement2.cloneNode(true);
+  
+  
+  //style and change the text of the new button
+  newElement2.style.backgroundImage = "None";
+  newElement2.style.fontSize = "18px";
+  newElement2.style.color = "white";
+  newElement2.style.border = "none";
+  newElement2.style.backgroundColor = "black";
+  newElement2.style.width = "auto";
+  newElement2.innerText = "Cool Maths Games Hacks - Calicat_\n\nNo more ads\n\nFree fullscreen";
+  
+  oldElement2.parentNode.replaceChild(newElement2, oldElement2);
+}
 
+function init () {
+  
+}
+
+if (window.location.hostname == "code.digimaker.com.au") {
+  init();
+} else {
+  
+}
